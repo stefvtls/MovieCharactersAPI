ffdb2cee436ddc6f155c22ce6fa12b1f41423245
stefvtls <stefvtls@gmail.com>
2023-01-30 17:12:18 +0100

    FEAT: added delete method to delete a franchise in the franchise service

--- MovieCharactersAPI/src/main/java/no/noroff/moviecharactersapi/runner/Runner.java
+++ MovieCharactersAPI/src/main/java/no/noroff/moviecharactersapi/runner/Runner.java
@@ -2,21 +2,24 @@ package no.noroff.moviecharactersapi.runner;
 
 import jakarta.transaction.Transactional;
 import no.noroff.moviecharactersapi.repositories.FranchiseRepository;
-import no.noroff.moviecharactersapi.services.franchise.FranchiseService;
+import no.noroff.moviecharactersapi.models.Franchise;
+import no.noroff.moviecharactersapi.services.franchise.FranchiseServiceImpl;
+import no.noroff.moviecharactersapi.services.movie.MovieServiceImpl;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 
-
 @Component
 public class Runner implements ApplicationRunner {
     final FranchiseRepository franchiseRepo;
-    private final FranchiseService franchiseService;
+    private final FranchiseServiceImpl franchiseServiceImpl;
+    private final MovieServiceImpl movieServiceImpl;
 
-    public Runner(FranchiseRepository franchiseRepo, FranchiseService franchiseService) {
+    public Runner(FranchiseRepository franchiseRepo, FranchiseServiceImpl franchiseServiceImpl, MovieServiceImpl movieServiceImpl) {
         this.franchiseRepo = franchiseRepo;
-        this.franchiseService = franchiseService;
+        this.franchiseServiceImpl = franchiseServiceImpl;
+        this.movieServiceImpl = movieServiceImpl;
     }
 
 
@@ -25,10 +28,16 @@ public class Runner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
 
 
-        System.out.println(franchiseRepo.getAllCharacterIdsInFranchise(1));
-        System.out.println(franchiseService.getAllCharactersInFranchise(1));
-        franchiseService.getAllCharactersInFranchise(1).forEach(s -> System.out.println(s.getName()));
-        System.out.println(franchiseService.getAllMoviesInFranchise(1));
-        franchiseService.getAllMoviesInFranchise(1).forEach(s-> System.out.println(s.getTitle()));
+//        System.out.println(franchiseRepo.getAllCharacterIdsInFranchise(1));
+//        System.out.println(franchiseServiceImpl.getAllCharactersInFranchise(1));
+//        franchiseServiceImpl.getAllCharactersInFranchise(1).forEach(s -> System.out.println(s.getName()));
+//        System.out.println(franchiseServiceImpl.getAllMoviesInFranchise(1));
+//        franchiseServiceImpl.getAllMoviesInFranchise(1).forEach(s-> System.out.println(s.getTitle()));
+
+//        franchiseServiceImpl.deleteById(2);
+        Franchise fr = franchiseServiceImpl.findById(1);
+        franchiseServiceImpl.delete(fr);
+
+
     }
 }
--- MovieCharactersAPI/src/main/java/no/noroff/moviecharactersapi/services/franchise/FranchiseServiceImpl.java
+++ MovieCharactersAPI/src/main/java/no/noroff/moviecharactersapi/services/franchise/FranchiseServiceImpl.java
@@ -4,6 +4,7 @@ import no.noroff.moviecharactersapi.models.Franchise;
 import no.noroff.moviecharactersapi.models.Movie;
 import no.noroff.moviecharactersapi.models.Character;
 import no.noroff.moviecharactersapi.repositories.FranchiseRepository;
+import no.noroff.moviecharactersapi.repositories.MovieRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -17,10 +18,12 @@ public class FranchiseServiceImpl implements FranchiseService{
 
     private final Logger logger = LoggerFactory.getLogger(FranchiseServiceImpl.class);
     private final FranchiseRepository franchiseRepository;
+    private final MovieRepository movieRepository;
 
 
-    public FranchiseServiceImpl(FranchiseRepository franchiseRepository) {
+    public FranchiseServiceImpl(FranchiseRepository franchiseRepository, MovieRepository movieRepository) {
         this.franchiseRepository = franchiseRepository;
+        this.movieRepository = movieRepository;
     }
 
 
@@ -40,6 +43,7 @@ public class FranchiseServiceImpl implements FranchiseService{
         return franchiseRepository.save(entity);
     }
 
+
     @Override
     public Franchise update(Franchise entity) {
         return franchiseRepository.save(entity);
@@ -48,21 +52,21 @@ public class FranchiseServiceImpl implements FranchiseService{
 
     @Override
     public void deleteById(Integer franchiseId) {
-        // find all movies where my franchise is
-
-        //set it to null
-        // delete franchise
-
-
+        if(franchiseRepository.existsById(franchiseId)) {
+            Franchise franchise = franchiseRepository.findById(franchiseId).get();
+            franchise.getMovies().forEach(m -> m.setFranchise(null));
+            franchise.getMovies().forEach(m -> movieRepository.save(m));
+            franchiseRepository.deleteById(franchiseId);
+        }
+        else
+            logger.warn("No franchise exists with ID: " + franchiseId);
     }
 
     @Override
     public void delete(Franchise entity) {
-
+        int franchiseId = entity.getId();
+        deleteById(franchiseId);
     }
-
-
-
 
 
 
