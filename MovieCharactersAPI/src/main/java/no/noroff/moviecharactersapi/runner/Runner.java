087e4a7e25bf80ac171749becdffbb2bf9ac439f
stefvtls <stefvtls@gmail.com>
2023-01-30 16:36:03 +0100

    FEAT: added franchise service with Getter CRUD get methods and get AllCharacters and AllMovies methods

--- MovieCharactersAPI/src/main/java/no/noroff/moviecharactersapi/runner/Runner.java
+++ MovieCharactersAPI/src/main/java/no/noroff/moviecharactersapi/runner/Runner.java
@@ -1,6 +1,8 @@
 package no.noroff.moviecharactersapi.runner;
 
+import jakarta.transaction.Transactional;
 import no.noroff.moviecharactersapi.repositories.FranchiseRepository;
+import no.noroff.moviecharactersapi.services.franchise.FranchiseService;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -10,16 +12,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class Runner implements ApplicationRunner {
     final FranchiseRepository franchiseRepo;
+    private final FranchiseService franchiseService;
 
-    public Runner(FranchiseRepository franchiseRepo) {
+    public Runner(FranchiseRepository franchiseRepo, FranchiseService franchiseService) {
         this.franchiseRepo = franchiseRepo;
+        this.franchiseService = franchiseService;
     }
 
 
+    @Transactional
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
 
         System.out.println(franchiseRepo.getAllCharacterIdsInFranchise(1));
+        System.out.println(franchiseService.getAllCharactersInFranchise(1));
+        franchiseService.getAllCharactersInFranchise(1).forEach(s -> System.out.println(s.getName()));
+        System.out.println(franchiseService.getAllMoviesInFranchise(1));
+        franchiseService.getAllMoviesInFranchise(1).forEach(s-> System.out.println(s.getTitle()));
     }
 }
---
+++ MovieCharactersAPI/src/main/java/no/noroff/moviecharactersapi/services/franchise/FranchiseService.java
@@ -0,0 +1,16 @@
+package no.noroff.moviecharactersapi.services.franchise;
+
+import no.noroff.moviecharactersapi.models.Character;
+import no.noroff.moviecharactersapi.models.Movie;
+import no.noroff.moviecharactersapi.services.CrudService;
+import no.noroff.moviecharactersapi.models.Franchise;
+
+import java.util.Set;
+
+public interface FranchiseService extends CrudService<Franchise, Integer> {
+
+
+    Set<Movie> getAllMoviesInFranchise(int franchiseId);
+    Set<Character> getAllCharactersInFranchise(int franchiseId);
+
+}
