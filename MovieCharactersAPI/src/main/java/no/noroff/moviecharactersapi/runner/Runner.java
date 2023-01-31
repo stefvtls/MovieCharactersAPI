1d09aa6ebbca5473e14f24dda24d9759e6a3dfcb
stefvtls <stefvtls@gmail.com>
2023-01-31 17:35:29 +0100

    FEAT: added custom exceptions: FranchiseNotFound, added delete method to franchise controller

--- MovieCharactersAPI/src/main/java/no/noroff/moviecharactersapi/runner/Runner.java
+++ MovieCharactersAPI/src/main/java/no/noroff/moviecharactersapi/runner/Runner.java
@@ -1,41 +1,41 @@
-package no.noroff.moviecharactersapi.runner;
-
-import jakarta.transaction.Transactional;
-import no.noroff.moviecharactersapi.services.character.CharacterServiceImpl;
-import no.noroff.moviecharactersapi.services.franchise.FranchiseServiceImpl;
-import no.noroff.moviecharactersapi.services.movie.MovieServiceImpl;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-
-
-@Component
-public class Runner implements ApplicationRunner {
-
-    private final CharacterServiceImpl characterServiceImpl;
-    private final FranchiseServiceImpl franchiseServiceImpl;
-    private final MovieServiceImpl movieServiceImpl;
-
-
-    public Runner(CharacterServiceImpl characterServiceImpl, FranchiseServiceImpl franchiseServiceImpl, MovieServiceImpl movieServiceImpl) {
-        this.characterServiceImpl = characterServiceImpl;
-        this.franchiseServiceImpl = franchiseServiceImpl;
-        this.movieServiceImpl = movieServiceImpl;
-    }
-
-
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-
-
-
-//        System.out.println(franchiseServiceImpl.getAllCharactersInFranchise(1));
-//        franchiseServiceImpl.getAllCharactersInFranchise(1).forEach(s -> System.out.println(s.getName()));
-//        System.out.println(franchiseServiceImpl.getAllMoviesInFranchise(1));
-//        franchiseServiceImpl.getAllMoviesInFranchise(1).forEach(s-> System.out.println(s.getTitle()));
-//        franchiseServiceImpl.deleteById(2);
-
-//        characterServiceImpl.deleteById(1);
-    }
-}
+//package no.noroff.moviecharactersapi.runner;
+//
+//import jakarta.transaction.Transactional;
+//import no.noroff.moviecharactersapi.services.character.CharacterServiceImpl;
+//import no.noroff.moviecharactersapi.services.franchise.FranchiseServiceImpl;
+//import no.noroff.moviecharactersapi.services.movie.MovieServiceImpl;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.stereotype.Component;
+//
+//
+//@Component
+//public class Runner implements ApplicationRunner {
+//
+//    private final CharacterServiceImpl characterServiceImpl;
+//    private final FranchiseServiceImpl franchiseServiceImpl;
+//    private final MovieServiceImpl movieServiceImpl;
+//
+//
+//    public Runner(CharacterServiceImpl characterServiceImpl, FranchiseServiceImpl franchiseServiceImpl, MovieServiceImpl movieServiceImpl) {
+//        this.characterServiceImpl = characterServiceImpl;
+//        this.franchiseServiceImpl = franchiseServiceImpl;
+//        this.movieServiceImpl = movieServiceImpl;
+//    }
+//
+//
+//
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
+//
+//
+//
+////        System.out.println(franchiseServiceImpl.getAllCharactersInFranchise(1));
+////        franchiseServiceImpl.getAllCharactersInFranchise(1).forEach(s -> System.out.println(s.getName()));
+////        System.out.println(franchiseServiceImpl.getAllMoviesInFranchise(1));
+////        franchiseServiceImpl.getAllMoviesInFranchise(1).forEach(s-> System.out.println(s.getTitle()));
+////        franchiseServiceImpl.deleteById(2);
+//
+////        characterServiceImpl.deleteById(1);
+//    }
+//}
